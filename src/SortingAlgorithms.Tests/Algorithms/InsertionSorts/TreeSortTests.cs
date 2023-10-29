6f10cbbbf597494d94f800973d024cb15643c476
boerdereinar <einardegruyl@gmail.com>
2023-10-29 21:10:35 +0100

    Binary Tree Sort

---
+++ src/SortingAlgorithms.Tests/Algorithms/InsertionSorts/TreeSortTests.cs
@@ -0,0 +1,29 @@
+using SortingAlgorithms.Algorithms.InsertionSorts;
+using SortingAlgorithms.DataStructures.Trees;
+
+namespace SortingAlgorithms.Tests.Algorithms.InsertionSorts;
+
+/// <summary>
+/// Tests for <see cref="TreeSort{TKey,TTree}"/>.
+/// </summary>
+public sealed class TreeSortTests
+{
+	/// <summary>
+	/// Tests if <see cref="TreeSort{TKey,TTree}.Default"/> returns the correct instance.
+	/// </summary>
+	[Fact]
+	public void Default_ReturnsDefaultInstance()
+	{
+		var instance = TreeSort<int, BinaryTree>.Default;
+		Assert.IsType<TreeSort<int, BinaryTree>>(instance);
+	}
+
+	/// <summary>
+	/// Tests for <see cref="BinaryTreeSort{TKey}"/>.
+	/// </summary>
+	public sealed class BinaryTreeSortTests : SortingAlgorithmTestBase<BinaryTreeSortTests>, ISortingAlgorithmTest
+	{
+		/// <inheritdoc />
+		protected override Type Type => typeof(BinaryTreeSort<>);
+	}
+}
