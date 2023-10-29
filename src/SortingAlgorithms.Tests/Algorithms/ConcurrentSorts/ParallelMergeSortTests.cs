2317f926fafe1d08dfd68df58c4e1cb4c1340fc2
boerdereinar <einardegruyl@gmail.com>
2023-10-29 20:57:30 +0100

    Parallel Merge Sort

---
+++ src/SortingAlgorithms.Tests/Algorithms/ConcurrentSorts/ParallelMergeSortTests.cs
@@ -0,0 +1,24 @@
+using SortingAlgorithms.Algorithms.ConcurrentSorts;
+using SortingAlgorithms.Tests.TestUtilities;
+
+namespace SortingAlgorithms.Tests.Algorithms.ConcurrentSorts;
+
+/// <summary>
+/// Tests for <see cref="ParallelMergeSort{TKey}"/>.
+/// </summary>
+public sealed class ParallelMergeSortTests : SortingAlgorithmTestBase<ParallelMergeSortTests>, ISortingAlgorithmTest
+{
+	/// <inheritdoc />
+	protected override Type Type => typeof(ParallelMergeSort<>);
+
+	/// <summary>
+	/// Tests if the constructor of <see cref="ParallelMergeSort{TKey}"/> throws an <see cref="ArgumentOutOfRangeException"/>.
+	/// </summary>
+	[Fact]
+	public void Constructor_InvalidMaxDegreeOfParallelism_ThrowsArgumentOutOfRangeException()
+	{
+		var ex = Assert.Throws<ArgumentOutOfRangeException>("maxDegreeOfParallelism", () => new ParallelMergeSort<int>(0));
+		Assert.Equal(0, ex.ActualValue);
+		AssertException.MessageNotEmpty(ex);
+	}
+}
